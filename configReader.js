b52b784a09eac254b79a3655a64c9dfc8e87448b
pgrabarz <pgrabarz@pgs-soft.com>
2015-03-17 13:03:18 +0100

    initial commit

---
+++ configReader.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = function ($, config) {
+    var _ = $.lodash;
+
+    $.utils.checkMandatory(config, ['sources.babel']);
+
+    if(_.isUndefined(config.paths.tmp)) {
+        config.paths.app = 'tmp/';
+    }
+
+    config.babel = _.defaults(config.babel || {}, {
+        // defaults
+    });
+
+    config.tasks = _.defaults(config.tasks || {}, {
+        babel: 'babel',
+        watchBabel: 'watch:babel'
+    });
+
+    return config;
+};
